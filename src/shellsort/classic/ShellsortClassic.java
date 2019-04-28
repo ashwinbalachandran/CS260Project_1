b1706a3fb43e9c0e69775c0bef52932b23b06fbb
ashwinbalachandran <ash20win@gmail.com>
2019-04-28 14:46:03 -0700

    Commit with dependency errors for log4j.

--- src/shellsort/classic/ShellsortClassic.java
+++ src/shellsort/classic/ShellsortClassic.java
@@ -5,34 +5,7 @@ import java.util.ArrayList;
 import insertionsort.general.InsertionSort;
 
 public class ShellsortClassic {
-    public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<>();
-        ShellsortClassic ssc = new ShellsortClassic();
-        if (args.length > 0) {
-            try {
-                for (String n : args) {
-                    int num = Integer.parseInt(n);
-                    numbers.add(num);
-                }
-            } catch (NumberFormatException nfe) {
-                System.err.println("Invalid input in arguments :)");
-                System.err.println(nfe.toString());
-            }
-        } else {
-            numbers.add(11);
-            numbers.add(21);
-            numbers.add(13);
-            numbers.add(41);
-            numbers.add(15);
-            numbers.add(61);
-            numbers.add(17);
-            numbers.add(81);
-            numbers.add(19);
-            numbers = ssc.shellSort(numbers);
-            ssc.printSortedList(numbers);
-        }
-    }
-    private void printSortedList(ArrayList<Integer> numbers) {
+    public void printSortedList(ArrayList<Integer> numbers) {
     	for(int n:numbers) {
     		System.out.print(n+" ");
     	}
