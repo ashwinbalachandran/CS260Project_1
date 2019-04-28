b5aa59a7f3c31584d9d52d1b1dcf6c02445b57b7
ashwinbalachandran <ash20win@gmail.com>
2019-04-28 13:34:42 -0700

    Completed Classic Insertion Sort

--- src/shellsort/classic/ShellsortClassic.java
+++ src/shellsort/classic/ShellsortClassic.java
@@ -2,9 +2,12 @@ package shellsort.classic;
 
 import java.util.ArrayList;
 
+import insertionsort.general.InsertionSort;
+
 public class ShellsortClassic {
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<>();
+        ShellsortClassic ssc = new ShellsortClassic();
         if (args.length > 0) {
             try {
                 for (String n : args) {
@@ -25,14 +28,26 @@ public class ShellsortClassic {
             numbers.add(17);
             numbers.add(81);
             numbers.add(19);
-            shellSort(numbers);
+            numbers = ssc.shellSort(numbers);
+            ssc.printSortedList(numbers);
         }
     }
-    public static void shellSort(ArrayList<Integer> numbers){
-    	for(int i = 0;i<numbers.size();i++) {
-    		
+    private void printSortedList(ArrayList<Integer> numbers) {
+    	for(int n:numbers) {
+    		System.out.print(n+" ");
     	}
-    	
+    	System.out.println("");
+	}
+	public ArrayList<Integer> shellSort(ArrayList<Integer> numbers){
+    	ArrayList<Integer> gaps = new ArrayList<>();
+    	InsertionSort is = new InsertionSort();
+    	int n = numbers.size();
+    	for(int i = 1;i<=(Math.log(n)/Math.log(2));i++)
+    		gaps.add((n/(int)Math.pow(2, i)));
+    	for(int g:gaps) {
+    		numbers = is.insertionSort(numbers, g);
+    	}
+    	return numbers;
     }
 
 }
